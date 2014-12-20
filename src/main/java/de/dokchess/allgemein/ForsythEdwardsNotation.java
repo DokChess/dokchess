a296b41eb2a58798d1cf891c92e949dc0f6a057e
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-20 12:42:33 +0100

    Neu formattiert wegen Tabs

--- src/main/java/de/dokchess/allgemein/ForsythEdwardsNotation.java
+++ src/main/java/de/dokchess/allgemein/ForsythEdwardsNotation.java
@@ -26,139 +26,139 @@ import java.util.EnumSet;
  * Die Forsyth-Edwards-Notation (FEN) ist eine Kurznotation, mit der jede
  * beliebige Brettstellung im Schach als eine Zeichenkette niedergeschrieben
  * werden kann.
- * 
+ *
  * @author StefanZ
  */
 class ForsythEdwardsNotation {
 
-	private Felder() {
-	}
+    private Felder() {
+    }
 
-	public static void fromString(Stellung pos, String fen) {
+    public static void fromString(Stellung pos, String fen) {
 
-		String[] gruppen = fen.split(" ");
+        String[] gruppen = fen.split(" ");
 
-		int reihe = 0;
-		int linie = 0;
+        int reihe = 0;
+        int linie = 0;
 
-		String figuren = gruppen[0];
+        String figuren = gruppen[0];
 
-		for (int i = 0; i < figuren.length(); ++i) {
+        for (int i = 0; i < figuren.length(); ++i) {
 
-			char figur = figuren.charAt(i);
+            char figur = figuren.charAt(i);
 
-			if (Character.isDigit(figur)) {
-				int n = Integer.parseInt("" + figur);
-				for (int j = 0; j < n; ++j) {
-					pos.setFigur(reihe, linie, null);
-					linie++;
-				}
-			} else if (figur == '/') {
-				reihe++;
-				linie = 0;
-			} else {
-				FigurenArt pieceType = FigurenArt.ausBuchstabe(figur);
-				Farbe side = Character.isUpperCase(figur) ? Farbe.WEISS
-						: Farbe.SCHWARZ;
-				Figur p = new Figur(pieceType, side);
-				pos.setFigur(reihe, linie, p);
-				linie++;
-			}
-		}
-
-		String amZug = gruppen[1];
-		char side = amZug.charAt(0);
-		switch (side) {
-		case 'w':
-			pos.setAmZug(Farbe.WEISS);
-			break;
-		case 'b':
-			pos.setAmZug(Farbe.SCHWARZ);
-			break;
-		}
-		
-		String rochadeRecht = gruppen[2];
-		if (rochadeRecht.equals("-")) {
-			pos.setRochadeRechte(EnumSet.noneOf(RochadeRecht.class));
-		} else {
-			EnumSet<RochadeRecht> rechte = EnumSet.noneOf(RochadeRecht.class);
-			for (int i=0; i<rochadeRecht.length(); ++i) {
-				char c = rochadeRecht.charAt(i);
-				rechte.add(RochadeRecht.ausBuchstabe(c));
-			}
-			pos.setRochadeRechte(rechte);
-		}
-		
-		String enPassant = gruppen[3];
-		if (enPassant.equals("-")) {
-			pos.setEnPassantFeld(null);
-		} else {
-			Feld enPassantFeld = new Feld(enPassant);
-			pos.setEnPassantFeld(enPassantFeld);
-		}
-	}
-
-	public static String toString(Stellung position) {
-
-		StringBuffer sb = new StringBuffer();
-
-		for (int reihe = 0; reihe < 8; ++reihe) {
-			int leer = 0;
-			for (int linie = 0; linie < 8; ++linie) {
-
-				Figur piece = position.getFigur(reihe, linie);
-				if (piece == null) {
-					leer++;
-				} else {
-					if (leer > 0) {
-						sb.append(leer);
-						leer = 0;
-					}
-					sb.append(piece.alsBuchstabe());
-				}
-			}
-			if (leer > 0) {
-				sb.append(leer);
-			}
-			if (reihe < 7) {
-				sb.append("/");
-			}
-		}
-
-		sb.append(" ");
-
-		switch (position.getAmZug()) {
-		case WEISS:
-			sb.append('w');
-			break;
-		case SCHWARZ:
-			sb.append('b');
-			break;
-		}
-
-		sb.append(" ");
-
-		if (position.getRochadeRechte().size() == 0) {
-			sb.append("-");
-		} else {
-			for (RochadeRecht r : RochadeRecht.values()) {
-				if (position.getRochadeRechte().contains(r)) {
-					sb.append(r.alsBuchstabe());
-				}
-			}
-		}
-
-		sb.append(" ");
-        if (position.getEnPassantFeld() == null) {
-        	sb.append("-");
-        } else {
-        	sb.append(position.getEnPassantFeld());
+            if (Character.isDigit(figur)) {
+                int n = Integer.parseInt("" + figur);
+                for (int j = 0; j < n; ++j) {
+                    pos.setFigur(reihe, linie, null);
+                    linie++;
+                }
+            } else if (figur == '/') {
+                reihe++;
+                linie = 0;
+            } else {
+                FigurenArt pieceType = FigurenArt.ausBuchstabe(figur);
+                Farbe side = Character.isUpperCase(figur) ? Farbe.WEISS
+                        : Farbe.SCHWARZ;
+                Figur p = new Figur(pieceType, side);
+                pos.setFigur(reihe, linie, p);
+                linie++;
+            }
         }
-		
-		// TODO: Richtig implementieren
-		
-		sb.append(" 0 1");
 
-		return sb.toString();
-	}
+        String amZug = gruppen[1];
+        char side = amZug.charAt(0);
+        switch (side) {
+            case 'w':
+                pos.setAmZug(Farbe.WEISS);
+                break;
+            case 'b':
+                pos.setAmZug(Farbe.SCHWARZ);
+                break;
+        }
+
+        String rochadeRecht = gruppen[2];
+        if (rochadeRecht.equals("-")) {
+            pos.setRochadeRechte(EnumSet.noneOf(RochadeRecht.class));
+        } else {
+            EnumSet<RochadeRecht> rechte = EnumSet.noneOf(RochadeRecht.class);
+            for (int i = 0; i < rochadeRecht.length(); ++i) {
+                char c = rochadeRecht.charAt(i);
+                rechte.add(RochadeRecht.ausBuchstabe(c));
+            }
+            pos.setRochadeRechte(rechte);
+        }
+
+        String enPassant = gruppen[3];
+        if (enPassant.equals("-")) {
+            pos.setEnPassantFeld(null);
+        } else {
+            Feld enPassantFeld = new Feld(enPassant);
+            pos.setEnPassantFeld(enPassantFeld);
+        }
+    }
+
+    public static String toString(Stellung position) {
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int reihe = 0; reihe < 8; ++reihe) {
+            int leer = 0;
+            for (int linie = 0; linie < 8; ++linie) {
+
+                Figur piece = position.getFigur(reihe, linie);
+                if (piece == null) {
+                    leer++;
+                } else {
+                    if (leer > 0) {
+                        sb.append(leer);
+                        leer = 0;
+                    }
+                    sb.append(piece.alsBuchstabe());
+                }
+            }
+            if (leer > 0) {
+                sb.append(leer);
+            }
+            if (reihe < 7) {
+                sb.append("/");
+            }
+        }
+
+        sb.append(" ");
+
+        switch (position.getAmZug()) {
+            case WEISS:
+                sb.append('w');
+                break;
+            case SCHWARZ:
+                sb.append('b');
+                break;
+        }
+
+        sb.append(" ");
+
+        if (position.getRochadeRechte().size() == 0) {
+            sb.append("-");
+        } else {
+            for (RochadeRecht r : RochadeRecht.values()) {
+                if (position.getRochadeRechte().contains(r)) {
+                    sb.append(r.alsBuchstabe());
+                }
+            }
+        }
+
+        sb.append(" ");
+        if (position.getEnPassantFeld() == null) {
+            sb.append("-");
+        } else {
+            sb.append(position.getEnPassantFeld());
+        }
+
+        // TODO: Richtig implementieren
+
+        sb.append(" 0 1");
+
+        return sb.toString();
+    }
 }
