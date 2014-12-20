8ec28dbb701dafea3916981899403c24d527f5e4
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-20 16:09:58 +0100

    Interface fuer Stellungsbewertung

---
+++ src/main/java/de/dokchess/engine/bewertung/Bewertung.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.dokchess.engine.bewertung;
+
+import de.dokchess.allgemein.Farbe;
+import de.dokchess.allgemein.Stellung;
+
+/**
+ * Bewertung einer Stellung aus Sicht eines Spielers.
+ *
+ * @author StefanZ
+ */
+public interface Bewertung {
+    /**
+     * Liefert zur gegebenen Stellung eine Bewertung aus Sicht der angegebenen
+     * Spielerfarbe. Je h&ouml;her, desto besser.
+     *
+     * @param stellung zu bewertende Spielsituation
+     * @param ausSicht Spieler, aus desen Sicht bewertet wird
+     * @return Bewertung, 0 ist ausgeglichen, je h&ouml;her desto besser fuer
+     * den Spieler
+     */
+    int bewerteStellung(Stellung stellung, Farbe ausSicht);
+}
---
+++ src/main/java/de/dokchess/engine/bewertung/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Bewertet eine Stellung aus Sicht eines Spielers.
+ *
+ * Das Modul bewertet eine Stellung aus Sicht eines Spielers. Ergebnis ist eine Zahl,
+ * wobei 0 eine ausgeglichene Situation beschreibt, eine positive Zahl einen Vorteil für den
+ * Spieler, eine negative einen Nachteil. Je höher der Betrag, desto gr&uml;sser der Vor- bzw. Nachteil.
+ * Das Modul ermöglicht es so, Stellungen miteinander zu vergleichen.
+ */
+package de.dokchess.engine.bewertung;
