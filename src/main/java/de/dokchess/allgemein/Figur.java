679aad3abb382981a21f15ca360769b316cbef6a
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-20 12:43:45 +0100

    Datei neu formatiert wegen Tabs

--- src/main/java/de/dokchess/allgemein/Figur.java
+++ src/main/java/de/dokchess/allgemein/Figur.java
@@ -21,99 +21,96 @@ package de.dokchess.allgemein;
 
 /**
  * Figur im Schachspiel. Die Klasse ist unveraenderlich (immutable).
- * 
- * @author StefanZ 
+ *
+ * @author StefanZ
  */
 public final class Figur {
 
-	private final Farbe farbe;
+    private final Farbe farbe;
 
-	private final FigurenArt art;
+    private final FigurenArt art;
 
-	/**
-	 * Erzeugt eine unveraenderliche Figur.
-	 * 
-	 * @param art
-	 *            Art der Figur, etwa BAUER
-	 * @param farbe
-	 *            Farbe der Figut, etwa WEISS
-	 * 
-	 */
-	public Figur(FigurenArt art, Farbe farbe) {
-		this.art = art;
-		this.farbe = farbe;
-	}
+    /**
+     * Erzeugt eine unveraenderliche Figur.
+     *
+     * @param art   Art der Figur, etwa BAUER
+     * @param farbe Farbe der Figut, etwa WEISS
+     */
+    public Figur(FigurenArt art, Farbe farbe) {
+        this.art = art;
+        this.farbe = farbe;
+    }
 
-	/**
-	 * Die Art der Figur. Z.B. BAUER.
-	 * 
-	 * @return die Art.
-	 */
-	public FigurenArt getArt() {
-		return art;
-	}
+    /**
+     * Die Art der Figur. Z.B. BAUER.
+     *
+     * @return die Art.
+     */
+    public FigurenArt getArt() {
+        return art;
+    }
 
-	/**
-	 * Die Farbe der Figur. Z.B. WEISS.
-	 * 
-	 * @return die Farbe.
-	 */
-	public Farbe getFarbe() {
-		return farbe;
-	}
+    /**
+     * Die Farbe der Figur. Z.B. WEISS.
+     *
+     * @return die Farbe.
+     */
+    public Farbe getFarbe() {
+        return farbe;
+    }
 
-	/**
-	 * Darstellung der Figur gemaess FEN. Ein kleiner Buchstabe steht fuer eine
-	 * schwarze Figur, ein grosser fuer eine weisse. 'K' ist zum Beispiel ein
-	 * weisser Koenig, 'q' eine schwarze Dame.
-	 * 
-	 * @return Darstellung der Figur als Buchstabe
-	 */
-	public char alsBuchstabe() {
-		char buchstabe = art.getBuchstabe();
-		if (farbe == Farbe.SCHWARZ) {
-			buchstabe = Character.toLowerCase(buchstabe);
-		}
-		return buchstabe;
-	}
+    /**
+     * Darstellung der Figur gemaess FEN. Ein kleiner Buchstabe steht fuer eine
+     * schwarze Figur, ein grosser fuer eine weisse. 'K' ist zum Beispiel ein
+     * weisser Koenig, 'q' eine schwarze Dame.
+     *
+     * @return Darstellung der Figur als Buchstabe
+     */
+    public char alsBuchstabe() {
+        char buchstabe = art.getBuchstabe();
+        if (farbe == Farbe.SCHWARZ) {
+            buchstabe = Character.toLowerCase(buchstabe);
+        }
+        return buchstabe;
+    }
 
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append(farbe);
-		if (art == FigurenArt.DAME) {
-			sb.append("E ");
-		} else {
-			sb.append("ER ");
-		}
-		sb.append(art);
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(farbe);
+        if (art == FigurenArt.DAME) {
+            sb.append("E ");
+        } else {
+            sb.append("ER ");
+        }
+        sb.append(art);
 
-		return sb.toString();
-	}
+        return sb.toString();
+    }
 
-	@Override
-	public int hashCode() {
-		return (farbe.ordinal() * 10) + art.ordinal();
-	}
+    @Override
+    public int hashCode() {
+        return (farbe.ordinal() * 10) + art.ordinal();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Figur andereFigur = (Figur) obj;
-		if (art != andereFigur.art) {
-			return false;
-		}
-		if (farbe != andereFigur.farbe) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Figur andereFigur = (Figur) obj;
+        if (art != andereFigur.art) {
+            return false;
+        }
+        if (farbe != andereFigur.farbe) {
+            return false;
+        }
+        return true;
+    }
 }
