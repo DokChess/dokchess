f21c4d3a146ae16502e173df333c0ac3548ef3ec
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-20 23:15:15 +0100

    Neu formattiert.

--- src/main/java/de/dokchess/engine/AusBibliothek.java
+++ src/main/java/de/dokchess/engine/AusBibliothek.java
@@ -1,17 +1,18 @@
-/* Copyright 2010, 2011, 2012 Stefan Zoerner
- * 
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
  * This file is part of DokChess.
- * 
+ *
  * DokChess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * DokChess is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,17 +24,17 @@ import de.dokchess.eroeffnung.Eroeffnungsbibliothek;
 
 class AusBibliothek extends ZugAuswaehlen {
 
-	private Eroeffnungsbibliothek bibliothek;
+    private Eroeffnungsbibliothek bibliothek;
 
-	public AusBibliothek(Eroeffnungsbibliothek bibliothek,
-			ZugAuswaehlen nachfolger) {
-		super(nachfolger);
-		this.bibliothek = bibliothek;
-	}
+    public AusBibliothek(Eroeffnungsbibliothek bibliothek,
+                         ZugAuswaehlen nachfolger) {
+        super(nachfolger);
+        this.bibliothek = bibliothek;
+    }
 
-	@Override
-	public Zug waehleZug(Stellung stellung) {
-		Zug zug = bibliothek.liefereZug(stellung);
-		return (zug != null) ? zug : super.waehleZug(stellung);
-	}
+    @Override
+    public Zug waehleZug(Stellung stellung) {
+        Zug zug = bibliothek.liefereZug(stellung);
+        return (zug != null) ? zug : super.waehleZug(stellung);
+    }
 }
