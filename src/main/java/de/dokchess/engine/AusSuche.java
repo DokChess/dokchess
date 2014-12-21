548f48d1ab366998a19343f4ce698851284f4e99
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-21 14:10:39 +0100

    CheckStyle: Replace all tab characters in this file by sequences of white-spaces.

--- src/main/java/de/dokchess/engine/AusSuche.java
+++ src/main/java/de/dokchess/engine/AusSuche.java
@@ -1,20 +1,22 @@
-/* Copyright 2010, 2011, 2012 Stefan Zoerner
- * 
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
  * This file is part of DokChess.
- * 
+ *
  * DokChess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * DokChess is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.dokchess.engine;
 
 import de.dokchess.allgemein.Stellung;
@@ -22,21 +24,21 @@ import de.dokchess.allgemein.Zug;
 import de.dokchess.engine.zugauswahl.ZugAuswahl;
 
 class AusSuche extends ZugAuswaehlen {
-	
-	private ZugAuswahl suche;
-	
-	public AusSuche(ZugAuswahl suche) {
-		this(suche, null);
-	}
-	
-	public AusSuche(ZugAuswahl suche, ZugAuswaehlen nachfolger) {
-		super(nachfolger);
-		this.suche = suche;
-	}
-	
-	@Override
-	public Zug waehleZug(Stellung stellung) {
-		Zug zug = suche.ermittleZug(stellung);
-		return (zug != null) ? zug : super.waehleZug(stellung);
-	}
+
+    private ZugAuswahl suche;
+
+    public AusSuche(ZugAuswahl suche) {
+        this(suche, null);
+    }
+
+    public AusSuche(ZugAuswahl suche, ZugAuswaehlen nachfolger) {
+        super(nachfolger);
+        this.suche = suche;
+    }
+
+    @Override
+    public Zug waehleZug(Stellung stellung) {
+        Zug zug = suche.ermittleZug(stellung);
+        return (zug != null) ? zug : super.waehleZug(stellung);
+    }
 }
--- src/main/java/de/dokchess/engine/DefaultEngine.java
+++ src/main/java/de/dokchess/engine/DefaultEngine.java
@@ -1,20 +1,22 @@
-/* Copyright 2010, 2011, 2012 Stefan Zoerner
- * 
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
  * This file is part of DokChess.
- * 
+ *
  * DokChess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * DokChess is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.dokchess.engine;
 
 import de.dokchess.allgemein.Stellung;
@@ -26,52 +28,52 @@ import de.dokchess.regeln.Spielregeln;
 
 /**
  * Default-Implementierung einer Engine.
- * 
+ * <p/>
  * Ben&ouml;tigt zum Arbeiten eine Implementierung der Spielregeln, die
  * Er&ouml;ffnungsbibliothek ist optional.
- * 
+ *
  * @author StefanZ
  */
 public class DefaultEngine implements Engine {
 
-	private Stellung stellung;
+    private Stellung stellung;
 
-	private ZugAuswaehlen zugAuswahl;
+    private ZugAuswaehlen zugAuswahl;
 
-	public DefaultEngine(Spielregeln spielregeln) {
-		this(spielregeln, null);
-	}
+    public DefaultEngine(Spielregeln spielregeln) {
+        this(spielregeln, null);
+    }
 
-	public DefaultEngine(Spielregeln spielregeln,
-			Eroeffnungsbibliothek eroeffnungsbibliothek) {
+    public DefaultEngine(Spielregeln spielregeln,
+                         Eroeffnungsbibliothek eroeffnungsbibliothek) {
 
-		this.stellung = new Stellung();
+        this.stellung = new Stellung();
 
-		MinimaxAlgorithmus minimax = new MinimaxAlgorithmus();
-		minimax.setTiefe(4);
-		minimax.setSpielregeln(spielregeln);
-		minimax.setBewertung(new ReineMaterialBewertung());
+        MinimaxAlgorithmus minimax = new MinimaxAlgorithmus();
+        minimax.setTiefe(4);
+        minimax.setSpielregeln(spielregeln);
+        minimax.setBewertung(new ReineMaterialBewertung());
 
-		AusSuche ausSuche = new AusSuche(minimax);
-		if (eroeffnungsbibliothek != null) {
-			this.zugAuswahl = new AusBibliothek(eroeffnungsbibliothek, ausSuche);
-		} else {
-			this.zugAuswahl = ausSuche;
-		}
-	}
+        AusSuche ausSuche = new AusSuche(minimax);
+        if (eroeffnungsbibliothek != null) {
+            this.zugAuswahl = new AusBibliothek(eroeffnungsbibliothek, ausSuche);
+        } else {
+            this.zugAuswahl = ausSuche;
+        }
+    }
 
-	@Override
-	public void figurenAufbauen(Stellung stellung) {
-		this.stellung = stellung;
-	}
+    @Override
+    public void figurenAufbauen(Stellung stellung) {
+        this.stellung = stellung;
+    }
 
-	@Override
-	public Zug ermittleDeinenZug() {
-		return zugAuswahl.waehleZug(stellung);
-	}
+    @Override
+    public Zug ermittleDeinenZug() {
+        return zugAuswahl.waehleZug(stellung);
+    }
 
-	@Override
-	public void ziehen(Zug zug) {
-		stellung = stellung.fuehreZugAus(zug);
-	}
+    @Override
+    public void ziehen(Zug zug) {
+        stellung = stellung.fuehreZugAus(zug);
+    }
 }
