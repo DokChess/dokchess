16a484d0260d280bc73a0cb3d486443e6da48f4d
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-12-24 10:53:33 +0100

    Sehr einfache Tests fuer die Default engine

---
+++ src/test/java/de/dokchess/engine/DefaultEngineTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2010-2014 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.dokchess.engine;
+
+import de.dokchess.allgemein.Stellung;
+import de.dokchess.allgemein.Zug;
+import de.dokchess.eroeffnung.polyglot.PolyglotOpeningBook;
+import de.dokchess.regeln.Spielregeln;
+import de.dokchess.regeln.SpielregelnImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import rx.Observable;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Einfache Tests, eher Richtung Smoke-Test ...
+ */
+public class DefaultEngineTest {
+
+    @Test
+    public void ohneEroeffnungsbiblithek() {
+
+        Spielregeln regeln = new SpielregelnImpl();
+        DefaultEngine engine = new DefaultEngine(regeln);
+
+        Stellung anfang = new Stellung();
+        engine.figurenAufbauen(anfang);
+
+        Observable<Zug> observable = engine.ermittleDeinenZug();
+        Assert.assertNotNull(observable);
+    }
+
+    @Test
+    public void mitEroeffnungsbiblithek() throws IOException {
+
+        InputStream is = getClass().getClassLoader().getResourceAsStream(
+                "de/dokchess/eroeffnung/polyglot/italienischePartie.bin");
+        PolyglotOpeningBook buch = new PolyglotOpeningBook(is);
+        is.close();
+
+        Spielregeln regeln = new SpielregelnImpl();
+        DefaultEngine engine = new DefaultEngine(regeln, buch);
+
+        Stellung anfang = new Stellung();
+        engine.figurenAufbauen(anfang);
+
+        Observable<Zug> observable = engine.ermittleDeinenZug();
+        Assert.assertNotNull(observable);
+    }
+}
