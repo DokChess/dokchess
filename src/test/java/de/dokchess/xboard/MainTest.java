0915308bd585862b06fd486f1e39a5c2be958782
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-01-31 08:54:18 +0100

    Test fuer xboard-Aspekt von Main

---
+++ src/test/java/de/dokchess/xboard/MainTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2010-2015 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.dokchess.xboard;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void xboardBauen() {
+        XBoard xboard = Main.xBoardBauen();
+        Assert.assertNotNull(xboard);
+    }
+}
