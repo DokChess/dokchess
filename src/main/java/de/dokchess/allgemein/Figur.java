ba32da02fe1c6d0ba210ee2ca4ca78d5561f31d9
Stefan Zörner <stefan.zoerner@embarc.de>
2021-03-03 20:04:22 +0100

    StringBuffer auf StringBuilder umgestellt.

--- src/main/java/de/dokchess/allgemein/Figur.java
+++ src/main/java/de/dokchess/allgemein/Figur.java
@@ -82,7 +82,7 @@ public final class Figur {
 
     @Override
     public String toString() {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append(farbe);
         if (art == FigurenArt.DAME) {
             sb.append("E ");
